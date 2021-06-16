ef8ec0f3808192c9674004f7a6cfd3fc4e5f11d8
riesinger <mail@pascal-riesinger.de>
2021-06-16 21:46:03 +0200

    Make amount and unit fields optional in more places

--- models/Ingredient.ts
+++ models/Ingredient.ts
@@ -5,7 +5,7 @@ export interface TranslatableIngredient {
   name: Localized<string>;
   scales: boolean;
   unit?: Unit;
-  amount: number;
+  amount?: number;
 }
 
 export type Ingredient = NonScalingIngredient | ScalingIngredient;
@@ -14,13 +14,13 @@ export type NonScalingIngredient = {
   name: string;
   scales?: false;
   unit?: Unit;
-  amount: number;
+  amount?: number;
 };
 
 export type ScalingIngredient = {
   name: string;
   scales: true;
-  unit: Unit;
+  unit?: Unit;
   amount: number;
 };
 
