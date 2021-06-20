f38ce519ba7e8ab7dad4e3a8ee164337d07e95d7
riesinger <mail@pascal-riesinger.de>
2021-06-20 23:49:59 +0200

    Make the size of IconForDiet adjustable

--- components/IconForDiet.tsx
+++ components/IconForDiet.tsx
@@ -9,23 +9,29 @@ import { useTranslation } from "next-i18next";
 import React from "react";
 import { Recipe } from "../models/Recipe";
 
-const IconForDiet = ({ diet }: { diet: Recipe["diet"] }) => {
+const IconForDiet = ({
+  diet,
+  size = 1,
+}: {
+  diet: Recipe["diet"];
+  size?: number;
+}) => {
   const { t } = useTranslation();
   switch (diet) {
     case "meat":
-      return <Icon path={mdiFoodSteak} title={t(`diet.${diet}`)} size={1} />;
+      return <Icon path={mdiFoodSteak} title={t(`diet.${diet}`)} size={size} />;
     case "fish":
-      return <Icon path={mdiFish} title={t(`diet.${diet}`)} size={1} />;
+      return <Icon path={mdiFish} title={t(`diet.${diet}`)} size={size} />;
     case "vegetarian":
       return (
         <Icon
           path={mdiAlphaVCircleOutline}
           title={t(`diet.${diet}`)}
-          size={1}
+          size={size}
         />
       );
     case "vegan":
-      return <Icon path={mdiLeaf} title={t(`diet.${diet}`)} size={1} />;
+      return <Icon path={mdiLeaf} title={t(`diet.${diet}`)} size={size} />;
   }
 };
 
