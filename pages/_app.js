db48f5ad63158f147bddb2fc0e063b517bb05786
riesinger <mail@pascal-riesinger.de>
2021-06-23 22:20:01 +0200

    Make the body full-height by default
    By doing this, the footer will be "sticky" at the bottom

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import "../styles/globals.css";
-import Head from "next/head";
 import { appWithTranslation } from "next-i18next";
+import Head from "next/head";
+import styled from "styled-components";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import "../styles/globals.css";
+
+const Main = styled.main`
+  flex: 1 0 auto;
+`;
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,9 +16,9 @@ function MyApp({ Component, pageProps }) {
         <link rel="stylesheet" href="/inter.css" />
       </Head>
       <Header></Header>
-      <main>
+      <Main>
         <Component {...pageProps} />
-      </main>
+      </Main>
       <Footer></Footer>
     </>
   );
