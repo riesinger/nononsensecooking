fb46b6f7c48b5be842a3cba1b6fb9869fd9d0567
riesinger <mail@pascal-riesinger.de>
2021-06-23 22:11:26 +0200

    Don't reload  recipes for today  on every load

--- pages/api/recipes/recommended/for-today.ts
+++ pages/api/recipes/recommended/for-today.ts
@@ -12,10 +12,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
 function shuffleArray(arr: any[]) {
   arr.sort(() => Math.random() - 0.5);
+  return arr;
+}
+
+let _recipesForTheDay = null;
+
+async function recipesForTheDay() {
+  if (!_recipesForTheDay) {
+    _recipesForTheDay = shuffleArray([...(await allRecipes())]).slice(0, 3);
+  }
+  return _recipesForTheDay;
 }
 
 export async function recipesOfTheDayForLanguage(language: SupportedLanguage) {
-  const recipes = [...(await allRecipes())];
-  shuffleArray(recipes);
-  return recipes.slice(0, 3).map(translateTo(language));
+  return (await recipesForTheDay()).map(translateTo(language));
 }
