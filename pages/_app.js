c528315d406903b4375f5ca8bd1267b51cab8ccd
riesinger <mail@pascal-riesinger.de>
2021-06-28 22:15:30 +0200

    Add a favicon
    This closes #42

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,27 @@ function MyApp({ Component, pageProps }) {
       <PageLoadingIndicator />
       <Head>
         <link rel="stylesheet" href="/inter.css" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5686f5" />
+        <meta name="msapplication-TileColor" content="#5686f5" />
+        <meta name="theme-color" content="#5686f5" />
       </Head>
       <Header></Header>
       <Main>
