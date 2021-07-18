d55bc047567d3beb80effd4dae62b3901436cb5f
riesinger <mail@pascal-riesinger.de>
2021-07-18 15:49:38 +0200

    Use different import for next.config.js

--- scripts/generate-recipe-index.mjs
+++ scripts/generate-recipe-index.mjs
@@ -2,7 +2,7 @@ import fs from "fs/promises";
 import path from "path";
 import slug from "slug";
 import YAML from "yaml";
-import config from "../next.config.js";
+import { default as config } from "../next.config.js";
 
 const basePath = "./public/recipes";
 const supportedLocales = config.i18n.locales;
