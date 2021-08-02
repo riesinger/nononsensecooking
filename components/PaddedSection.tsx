2915dc4cfcf48721323f1996df2f468eff01718a
riesinger <mail@pascal-riesinger.de>
2021-08-02 22:35:54 +0200

    Add width and title sizing option to PaddedSection

--- components/PaddedSection.tsx
+++ components/PaddedSection.tsx
@@ -1,26 +1,35 @@
 import { ReactNode } from "react";
 import styled from "styled-components";
 
-const StyledSection = styled.section`
+const StyledSection = styled.section<{ width?: "default" | "narrow" }>`
   width: 100%;
-  max-width: 2000px;
+  max-width: ${({ width }) => (width === "narrow" ? "700px" : "2000px")};
   margin: 0 auto 4rem auto;
   padding: 0 2rem;
   box-sizing: border-box;
 `;
 
-const Title = styled.h3`
-  font-weight: normal;
+const Title = styled.h3<{ smallHeadings?: boolean }>`
+  font-weight: ${({ smallHeadings }) => (smallHeadings ? "500" : "700")};
+  font-size: ${({ smallHeadings }) => (smallHeadings ? "1.75rem" : "2rem")};
+  color: var(--color-text-primary);
 `;
 
 type Props = {
   title?: string;
   children?: ReactNode;
+  width?: "default" | "narrow";
+  smallHeadings?: boolean;
 };
 
-export const PaddedSection = ({ title, children }: Props) => (
-  <StyledSection>
-    {title ? <Title>{title}</Title> : null}
+export const PaddedSection = ({
+  title,
+  children,
+  width,
+  smallHeadings,
+}: Props) => (
+  <StyledSection width={width}>
+    {title ? <Title smallHeadings={smallHeadings}>{title}</Title> : null}
     {children}
   </StyledSection>
 );
