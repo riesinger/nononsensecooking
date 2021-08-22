b1021730b13931d49562db6dec1edbb460362041
riesinger <mail@pascal-riesinger.de>
2021-08-22 15:49:30 +0200

    Generate a full recipe index used to redirect to other locales

--- scripts/generate-recipe-index.ts
+++ scripts/generate-recipe-index.ts
@@ -17,6 +17,7 @@ const fieldsToIncludeInIndex = [
 ];
 
 async function generateIndex() {
+  const allRecipes = {};
   for (const locale of supportedLocales) {
     console.log("🛠 Generating index for locale", locale);
     const recipes = await loadRecipesFromDisk(locale as SupportedLanguage);
@@ -31,7 +32,24 @@ async function generateIndex() {
     const targetFile = path.join(basePath, `index_${locale}.json`);
     console.log("   📝 Writing to", targetFile);
     await fs.writeFile(targetFile, JSON.stringify(trimmedRecipes));
+    for (let recipe of recipes) {
+      if (allRecipes[recipe.id]) {
+        allRecipes[recipe.id].availableIn.push(locale);
+        allRecipes[recipe.id].slugs.push({ locale, slug: recipe.slug });
+      } else {
+        allRecipes[recipe.id] = {
+          availableIn: [locale],
+          slugs: [{ locale, slug: recipe.slug }],
+          image: recipe.image,
+        };
+      }
+    }
   }
+  console.log("🛠 Generating the full index");
+  const fullIndex = path.join(basePath, "index.json");
+  console.log("   👀 Found", Object.keys(allRecipes).length, "recipes");
+  console.log("   📝 Writing to", fullIndex);
+  await fs.writeFile(fullIndex, JSON.stringify(allRecipes));
 }
 
 generateIndex().catch(console.error);
