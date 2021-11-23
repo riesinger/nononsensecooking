00872cbc4e19d55acb240a1e37c8bd1808c8c725
riesinger <mail@pascal-riesinger.de>
2021-11-23 20:32:12 +0100

    chore(copy): reflect switch to Plausible in legal text

--- pages/legal.tsx
+++ pages/legal.tsx
@@ -80,37 +80,27 @@ export default function Legal({}: InferGetStaticPropsType<
             durchlesen.
           </Trans>
         </Paragraph>
-        <SubHeading>{t("section.privacy.goatcounter.title")}</SubHeading>
+        <SubHeading>{t("section.privacy.plausible.title")}</SubHeading>
         <Paragraph>
-          <Trans t={t} i18nKey="section.privacy.goatcounter.content">
-            Wir nutzen eine vom Inhaber dieser Seite bereitgestellte
-            Installation von Goatcounter. Dabei handelt es sich um ein Werkzeug
-            zum Erfassen von Analysedaten von Websites. Anders als bei vielen
+          <Trans t={t} i18nKey="section.privacy.plausible.content">
+            Wir nutzen auf dieser Seite eine selbstverwaltete Installation von
+            Plausible Analytics. Dabei handelt es sich um ein Werkzeug zum
+            Erfassen von Analysedaten von Websites. nders als bei vielen
             sogenannten Analytics-Tools nutzen wir Goatcounter nicht dazu, um
             Dich über das Internet hinweg zu tracken. Wir sammeln ausschließlich
             auf dieser Website Nutzungsdaten und können Dich als Person mit
             diesen Daten auch nicht identifizieren. Die Nutzungsdaten werden auf
             einem Server in Deutschland gespeichert, womit sie den sehr hohen
             europäischen Ansprüchen an Datenschutz unterliegen. Sobald du diese
-            Seite besuchst, sammeln wir folgende Daten von deinem Gerät: Die URL
-            der aufgerufenen Seite, den Referrer Header, den User-Agent Header,
-            deine Bildschirmgröße, Deine Region, welche aus deiner IP-Adresse
-            geschätzt wird und ein Hash Deiner IP-Adresse, des User-Agent
-            Headers und einer zufälligen Zahl. Dabei wird weder die IP-Adresse,
-            noch der berechnete Hash jemals auf ein persistentes Medium
-            (Festplatte) geschrieben, sondern maximal 8 Stunden im
-            Arbeitsspeicher vorgehalten. Es werden keinerlei Informationen in
-            Deinem Browser abgelegt (Cookies, localStorage, ...). Die
-            Datenschutzerklärung von GoatCounter kannst du dir auch{" "}
-            <a href="https://www.goatcounter.com/privacy">hier</a> durchlesen.
-            Zusätzlich stellt der Entwickler von GoatCounter auch ein Dokument
-            bereit, welches die gesammelten Daten näher beschreibt. Dieses
-            Dokument kannst Du dir{" "}
-            <a href="https://www.goatcounter.com/gdpr">hier</a> ansehen.
-            Solltest Du dem Sammeln der beschriebenen Daten widersprechen, aber
-            die Seite trotzdem nutzen wollen, kannst Du einfach JavaScript in
-            Deinem Browser deaktivieren. Damit wird das Laden von GoatCounter
-            unterbunden.
+            Seite besuchst, sammeln wir folgende Daten: die Seiten-URL, den
+            Referrer-Header, deinen Browser, dein Betriebssystem, deinen
+            Gerätetyp (Desktop, Mobiltelefon, Tablet) und das Land, aus dem du
+            die Website aufrufst. Wenn du mehr über die Daten, die wir mit
+            Plausible Analytics sammeln erfahren willst und herausfinden
+            möchtest, warum wir damit nicht auf dich als Person schließen
+            können, kannst du dir{" "}
+            <a href="https://plausible.io/data-policy">hier</a> die Daten-Policy
+            von Plausible anschauen.
           </Trans>
         </Paragraph>
       </PaddedSection>
