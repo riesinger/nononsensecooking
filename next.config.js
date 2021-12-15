a0b56b78c53299371cdab69ffec997be3ae79d8b
riesinger <mail@pascal-riesinger.de>
2021-12-15 20:51:05 +0100

    feat: use SWC instead of babel
    This patch enables the Next.JS-native SWC transform for
    styled-components, enabling us to ditch babel, leading to faster
    compiles.
    While the styled-components transform is experimental, it seems like it
    is working just fine.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   i18n,
+  experimental: {
+    styledComponents: true,
+  },
   images: {
     formats: ["image/avif", "image/webp"],
   },
