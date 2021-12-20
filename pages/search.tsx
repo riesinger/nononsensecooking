f38f3542bc654e1e4ca370ce4571581be1d4bda3
riesinger <mail@pascal-riesinger.de>
2021-12-20 18:27:50 +0100

    fix: don't error on empty search terms
    The search page would error-out when the `query` parameter wan't passed.
    This has been fixed. Additionally, the experience on an "empty" search
    page has been improved in that a search bar is now show on it.
    
    This fixes #100

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,9 +1,11 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import styled from "styled-components";
 import DishList from "../components/DishList";
 import DishListItem from "../components/DishListItem";
 import { PaddedSection } from "../components/PaddedSection";
+import SearchBar from "../components/SearchBar";
 import SEO from "../components/SEO";
 import languageFrom from "../lib/languageFrom";
 import { queryParam } from "../lib/queryParameter";
@@ -11,6 +13,19 @@ import { sanitize, searchRecipes } from "./api/search";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const unsanitizedSearchTerm = queryParam("query").from(context);
+  if (!unsanitizedSearchTerm) {
+    return {
+      props: {
+        searchTerm: "",
+        results: [],
+        ...(await serverSideTranslations(context.locale, [
+          "common",
+          "header",
+          "footer",
+        ])),
+      },
+    };
+  }
   const searchTerm = sanitize(unsanitizedSearchTerm);
   const results = await searchRecipes(languageFrom(context), searchTerm);
   return {
@@ -26,11 +41,42 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   };
 };
 
+const Notice = styled.p`
+  text-align: center;
+  font-size: 1.5rem;
+`;
+
+const CenteredSection = styled.section`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 2rem;
+  width: 100%;
+  max-width: 700px;
+  margin: 25vh auto 0 auto;
+  padding: 0 2rem;
+  box-sizing: border-box;
+`;
+
 export default function Search({
   searchTerm,
   results,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const { t } = useTranslation("common");
+  const { t: th } = useTranslation("header");
+  if (!searchTerm) {
+    return (
+      <>
+        <SEO title={t("search.pagetitle")} />
+        <CenteredSection>
+          <Notice>Finde deine Lieblingsrezepte</Notice>
+          <SearchBar placeholder={th("searchbar.placeholder")} />
+        </CenteredSection>
+      </>
+    );
+  }
   return (
     <>
       <SEO title={t("search.pagetitle")} />
