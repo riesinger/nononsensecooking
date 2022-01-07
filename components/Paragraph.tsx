d730f77a630806837b010721f6073aa7ff8b6f1b
riesinger <mail@pascal-riesinger.de>
2022-01-07 21:50:49 +0100

    feat(components): new Paragraph component

---
+++ components/Paragraph.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Paragraph = styled.p`
+  max-width: 70ch;
+  line-height: 1.6;
+  color: var(--color-text-secondary);
+  margin-bottom: 1.5rem;
+`;
+
+export default Paragraph;
