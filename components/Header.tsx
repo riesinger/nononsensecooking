c87c88938b3bd717c7d841b842be953ec32aeecd
riesinger <mail@pascal-riesinger.de>
2023-07-15 19:22:07 +0200

    Migrate the header component to tailwindcss

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,125 +1,25 @@
 import { mdiChefHat, mdiMenu, mdiPotSteamOutline } from "@mdi/js";
 import Icon from "@mdi/react";
+import clsx from "clsx";
 import { useTranslation } from "next-i18next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import styled from "styled-components";
 import SearchBar from "./SearchBar";
 
-const StyledHeader = styled.header`
-  padding: 1rem 2rem;
-  margin: 0;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  gap: 1.5rem;
-  position: relative;
-`;
-
-const StyledHeading = styled.h1`
-  font-size: 1.3rem;
-  margin: 0;
-  font-weight: 400;
-  margin-left: 0.75rem;
-  display: none;
-
-  @media screen and (min-width: 400px) {
-    display: block;
-  }
-
-  @media screen and (min-width: 800px) {
-    font-size: 1.5rem;
-  }
-`;
-
-const HomeLink = styled(Link)`
-  display: flex;
-  align-items: center;
-  max-width: 100%;
-  cursor: pointer;
-  color: var(--color-text-primary);
-`;
-
-const IconContainer = styled.div`
-  background: var(--color-primary);
-  width: 3rem;
-  height: 3rem;
-  border-radius: var(--rounded);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: hsla(var(--palette-gray-00), 100%);
-`;
-
-const StyledNav = styled.nav`
-  display: flex;
-  align-items: center;
-  gap: 2rem;
-  flex-wrap: wrap;
-  max-width: 100%;
-`;
-
-const StyledLink = styled(Link)`
-  color: var(--color-text-secondary);
-  transition: color 0.1s linear;
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-
-  &:hover {
-    color: var(--color-primary);
-  }
-`;
-const MenuButton = styled.button`
-  outline: none;
-  appearance: none;
-  cursor: pointer;
-  /* background: var(--color-primary); */
-  background: none;
-  border: none;
-  /* color: var(--color-text-on-primary); */
-  color: var(--color-text-primary);
-  width: 3rem;
-  height: 3rem;
-  border-radius: var(--rounded);
-
-  @media screen and (min-width: 950px) {
-    display: none;
-  }
-`;
-
-const Menu = styled.div<{ open: boolean }>`
-  position: absolute;
-  z-index: 200;
-  top: 100%;
-  left: 0;
-  right: 0;
-  width: 100%;
-  box-shadow: var(--shadow);
-  background: var(--color-background);
-  padding: 2rem 2rem 2rem 2rem;
-  visibility: ${({ open }) => (open ? "visible" : "hidden")};
-  pointer-events: ${({ open }) => (open ? "auto" : "none")};
-  transition: all 0.15s ease-in;
-  transform: ${({ open }) => (open ? "scale(1)" : "scale(0.95)")};
-  opacity: ${({ open }) => (open ? 1 : 0)};
-  transform-origin: top center;
-
-  @media screen and (min-width: 950px) {
-    position: static;
-    visibility: visible;
-    pointer-events: all;
-    padding: 0;
-    opacity: 1;
-    transform: scale(1);
-    box-shadow: none;
-    width: auto;
-    top: 0%;
-  }
-`;
+const MenuContent = ({ t }) => (
+  <nav className="flex items-center gap-8 flex-wrap max-w-full">
+    <Link
+      className="text-neutral-600 hover:text-neutral-900 dark:text-neutral-300 dark:hover:text-neutral-100 flex items-cennter gap-2"
+      href="/r"
+      prefetch={false}
+    >
+      <Icon path={mdiPotSteamOutline} size={1} />
+      <span>{t("link.allrecipes")}</span>
+    </Link>
+    <SearchBar placeholder={t("searchbar.placeholder")} />
+  </nav>
+);
 
 const Header = () => {
   const { t } = useTranslation("header");
@@ -143,26 +43,45 @@ const Header = () => {
   }, [router]);
 
   return (
-    <StyledHeader>
-      <HomeLink href="/" passHref>
-        <IconContainer>
-          <Icon path={mdiChefHat} size={1.5} rotate={10} />
-        </IconContainer>
-        <StyledHeading>NoNonsenseCooking</StyledHeading>
-      </HomeLink>
-      <MenuButton onClick={toggleMenu}>
-        <Icon path={mdiMenu} size={1} />
-      </MenuButton>
-      <Menu open={menuOpen}>
-        <StyledNav>
-          <StyledLink href="/r" passHref prefetch={false}>
-            <Icon path={mdiPotSteamOutline} size={1} />
-            <span>{t("link.allrecipes")}</span>
-          </StyledLink>
-          <SearchBar placeholder={t("searchbar.placeholder")} />
-        </StyledNav>
-      </Menu>
-    </StyledHeader>
+    <header
+      className="px-8 py-4 m-0 w-full flex items-center justify-between flex-wrap
+    gap-6 relative"
+    >
+      <Link
+        className="flex items-center max-w-full cursor-pointer text-neutral-900 dark:text-neutral-100 gap-3"
+        href="/"
+      >
+        <div className="bg-brand w-12 h-12 rounded-md flex items-center justify-center">
+          <Icon
+            className="text-neutral-100"
+            path={mdiChefHat}
+            size={1.5}
+            rotate={10}
+          />
+        </div>
+        <h1 className="text-2xl m-0 font-medium tracking-tight hidden md:block">
+          NoNonsenseCooking
+        </h1>
+      </Link>
+      <button
+        className="outline-none appearance-none cursor-pointer bg-transparent border-none text-neutral-900 dark:text-neutral-100 w-12 h-12 md:hidden"
+        onClick={toggleMenu}
+      >
+        <Icon path={mdiMenu} size={1} className="ml-3" />
+      </button>
+      <div className="hidden md:block">
+        <MenuContent t={t} />
+      </div>
+      <div
+        className={clsx(
+          "absolute top-full z-10 left-0 right-0 w-full shadow-lg bg-neutral-100 dark:bg-neutral-900 p-8 transition md:hidden",
+          menuOpen && "visible pointer-events-auto opacity-100 scale-100",
+          !menuOpen && "invisible pointer-events-none opacity-0 scale-95"
+        )}
+      >
+        <MenuContent t={t} />
+      </div>
+    </header>
   );
 };
 
