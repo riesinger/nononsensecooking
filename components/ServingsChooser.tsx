4145a6b6ce631c6da61ca47fb7f6698cc2881949
riesinger <mail@pascal-riesinger.de>
2023-07-17 08:02:58 +0200

    Migrate ServingsChooser to tailwindcss

--- components/ServingsChooser.tsx
+++ components/ServingsChooser.tsx
@@ -1,42 +1,6 @@
 import { mdiMinus, mdiPlus } from "@mdi/js";
 import Icon from "@mdi/react";
 import { useTranslation } from "next-i18next";
-import styled from "styled-components";
-
-const IconButton = styled.button`
-  appearance: none;
-  border: none;
-  background: var(--color-background-alt);
-  color: var(--color-text-primary);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  border-radius: var(--rounded-full);
-  width: 2.5rem;
-  height: 2.5rem;
-`;
-
-const ChooserInputLine = styled.div`
-  display: flex;
-  gap: 1rem;
-  align-items: center;
-`;
-
-const Servings = styled.span`
-  font-size: 1.275rem;
-`;
-
-const Heading = styled.h5`
-  font-weight: 500;
-  font-size: 1.275rem;
-`;
-
-const StyledServingsChooser = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 2rem;
-`;
 
 interface Props {
   servings: number;
@@ -46,27 +10,29 @@ interface Props {
 const ServingsChooser = ({ servings, onServingsChanged }: Props) => {
   const { t } = useTranslation("recipe");
   return (
-    <StyledServingsChooser>
-      <Heading>{t("servings")}</Heading>
-      <ChooserInputLine>
-        <IconButton
+    <div className="flex items-center gap-8">
+      <h5 className="text-semibold text-xl">{t("servings")}</h5>
+      <div className="flex items-center gap-4">
+        <button
+          className="appearance-none border-none bg-neutral-50 hover:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700 rounded-full w-8 h-8 pl-1 ring-1 ring-neutral-950/20 dark:ring-neutral-50/20 shadow"
           onClick={function () {
             onServingsChanged(Math.max(1, servings - 1));
           }}
         >
           <Icon path={mdiMinus} size={1} title="Less servings" />
-        </IconButton>
+        </button>
 
-        <Servings>{servings}</Servings>
-        <IconButton
+        <span className="text-xl">{servings}</span>
+        <button
+          className="appearance-none border-none bg-neutral-50 hover:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700 rounded-full w-8 h-8 pl-1 ring-1 ring-neutral-950/20 dark:ring-neutral-50/20 shadow"
           onClick={function () {
             onServingsChanged(servings + 1);
           }}
         >
           <Icon path={mdiPlus} size={1} title="More servings" />
-        </IconButton>
-      </ChooserInputLine>
-    </StyledServingsChooser>
+        </button>
+      </div>
+    </div>
   );
 };
 
