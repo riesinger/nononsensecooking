39a433cc858738c8ccc3b63a33e8f73f9ba3afbc
riesinger <mail@pascal-riesinger.de>
2023-07-17 18:34:40 +0200

    Migrate PageLoadingIndicator to tailwindcss

--- components/PageLoadingIndicator.tsx
+++ components/PageLoadingIndicator.tsx
@@ -1,21 +1,6 @@
+import clsx from "clsx";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import styled from "styled-components";
-
-const Indicator = styled.div<{ isLoading: boolean }>`
-  width: 100%;
-  height: 5px;
-  background: var(--color-primary);
-  box-shadow: 0px 1px 15px hsla(var(--palette-blue-40), 85%);
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 50;
-  transition: transform 0.3s ease-in-out;
-  transform: scaleX(${({ isLoading }) => (isLoading ? "0.9" : 0)});
-  visibility: ${({ isLoading }) => (isLoading ? "visible" : "hidden")};
-  transform-origin: left;
-`;
 
 const PageLoadingIndicator = () => {
   const router = useRouter();
@@ -40,7 +25,15 @@ const PageLoadingIndicator = () => {
       router.events.off("routeChangeError", handleStop);
     };
   }, [router]);
-  return <Indicator isLoading={loading} />;
+  return (
+    <div
+      className={clsx(
+        "w-full h-1 bg-brand shadow shadow-brand absolute top-0 left-0 z-50 transition-transform duration-300 origin-left scale-x-0",
+        loading && "visible scale-x-90",
+        !loading && "invisible",
+      )}
+    />
+  );
 };
 
 export default PageLoadingIndicator;
