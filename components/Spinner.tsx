76dfbe323a1e69652199b62cf5477ecb0b3214b3
riesinger <mail@pascal-riesinger.de>
2023-07-18 22:12:23 +0200

    Migrate Spinner to tailwindcss

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,26 +1,5 @@
-import styled, { keyframes } from "styled-components";
-
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const SpinnerCircle = styled.div`
-  border-radius: 50%;
-  border-width: 4px;
-  border-color: var(--color-background-alt);
-  border-top-color: var(--color-primary);
-  border-style: solid;
-  width: 3rem;
-  height: 3rem;
-  animation: ${rotate} 1s linear infinite;
-`;
-
 export default function Spinner() {
-  return <SpinnerCircle />;
+  return (
+    <div className="rounded-full border-4 border-gray-200 dark:border-gray-800 border-t-brand border-solid w-12 h-12 animate-spin" />
+  );
 }
