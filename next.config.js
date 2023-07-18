0787b95ba11625a66d6b26b12cc1b36b7060a2e5
riesinger <mail@pascal-riesinger.de>
2023-07-18 22:23:24 +0200

    Remove styled-components

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@ const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   i18n,
-  compiler: {
-    styledComponents: true,
-  },
+  compiler: {},
   images: {
     formats: ["image/avif", "image/webp"],
   },
-  swcMinify: true
+  swcMinify: true,
 };
