b75642caa0e5e3fec71586b1023fd1c0e50415ba
riesinger <mail@pascal-riesinger.de>
2023-07-18 22:18:59 +0200

    Migrate PaddedSection to tailwindcss

--- components/PaddedSection.tsx
+++ components/PaddedSection.tsx
@@ -1,19 +1,4 @@
 import { ReactNode } from "react";
-import styled from "styled-components";
-
-const StyledSection = styled.section<{ width?: "default" | "narrow" }>`
-  width: 100%;
-  max-width: ${({ width }) => (width === "narrow" ? "700px" : "2000px")};
-  margin: 0 auto 4rem auto;
-  padding: 0 2rem;
-  box-sizing: border-box;
-`;
-
-const Title = styled.h3<{ smallHeadings?: boolean }>`
-  font-weight: ${({ smallHeadings }) => (smallHeadings ? "500" : "700")};
-  font-size: ${({ smallHeadings }) => (smallHeadings ? "1.75rem" : "2rem")};
-  color: var(--color-text-primary);
-`;
 
 type Props = {
   title?: string;
@@ -22,14 +7,16 @@ type Props = {
   smallHeadings?: boolean;
 };
 
-export const PaddedSection = ({
-  title,
-  children,
-  width,
-  smallHeadings,
-}: Props) => (
-  <StyledSection width={width}>
-    {title ? <Title smallHeadings={smallHeadings}>{title}</Title> : null}
+export const PaddedSection = ({ title, children, smallHeadings }: Props) => (
+  <section
+    className="w-full max-w-screen-2xl
+   mx-auto px-8 box-border"
+  >
+    {title ? (
+      <h3 className="font-bold text-2xl mt-8 mb-4 leading-snug text-neutral-950 dark:text-neutral-50">
+        {title}
+      </h3>
+    ) : null}
     {children}
-  </StyledSection>
+  </section>
 );
