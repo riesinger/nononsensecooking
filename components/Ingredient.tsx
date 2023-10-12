717268e2707a2fa0cd595dc0ebde85c8154d73f3
riesinger <mail@pascal-riesinger.de>
2023-10-12 21:39:33 +0200

    chore: switch to bun

--- components/Ingredient.tsx
+++ components/Ingredient.tsx
@@ -1,4 +1,4 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "next-i18next";
 import { Ingredient as IngredientModel } from "../models/Ingredient";
 import { Unit } from "../models/Unit";
 
