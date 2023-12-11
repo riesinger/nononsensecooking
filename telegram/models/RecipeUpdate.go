4ea89d0d2b2fe805654b848fdc1a8c94cb5bae90
riesinger <mail@pascal-riesinger.de>
2023-12-11 21:19:01 +0100

    chore: remove the telegram bot

--- telegram/models/RecipeUpdate.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type RecipeUpdate map[string]Update
-
-type Update struct {
-	Message string
-}
--- telegram/telegram/types.go
+++
@@ -1,9 +0,0 @@
-package telegram
-
-import "context"
-
-type UserStore interface {
-	Upsert(ctx context.Context, id int64, locale string) error
-	Subscribe(ctx context.Context, id int64) error
-	Unsubscribe(ctx context.Context, id int64) error
-}
